5d7a08bdb36373ffdb9ac89e159b7ed3028335c6
juan rodriguez <juanbrodriguez8@gmail.com>
2022-11-21 15:43:37 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
-Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  Rails.application.routes.draw do
+    # Generic syntax:
+    # verb "path", to: "controller#action"
+    get "ask", to: "questions#ask"
+    get "answer", to: "questions#answer"
+  end
+
